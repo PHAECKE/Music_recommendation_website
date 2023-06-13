74bde7745a3e959126797ab2e138313d40c37443
PHAECKE <francisamexo@yahoo.com>
2023-06-13 15:41:04 +0000

    ..

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # MySQL connection configuration
 mysql_host = 'localhost'
 mysql_user = 'root'
-mysql_password = 'prosperity'
+mysql_password = ''
 mysql_database = 'music_app'
 
 connection = mysql.connector.connect(
